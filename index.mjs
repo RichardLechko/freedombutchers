5f2894830b2fd020f802d1708cf892f3ffbd5e65
Richard Lechko <richardlechko04@gmail.com>
2024-05-25 20:59:25 -0500

    Final Commit (1.0)

--- index.mjs
+++ index.mjs
@@ -1,105 +1,174 @@
 import express from "express";
-import { fileURLToPath } from "url";
 import path from "path";
-import fetchYelpReviews from "./review.mjs";
-import phpExpress from "php-express";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+import cors from "cors";
+import axios from "axios";
+import http from "http";
+import fs from "fs";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+const __dirname = path.resolve();
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+dotenv.config();
 
-// Set up php-express
-const phpExpressEngine = new phpExpress({
-  binPath: "php-8.3.6/php-cgi", // Replace "path/to/php-cgi" with the actual path to your PHP CGI executable
-  root: path.join(__dirname, "freedombutchers"), // Root directory for PHP scripts
-});
+const allowedOrigins = [
+  "https://www.freedombutchers.richardlechko.com",
+  "http://www.freedombutchers.richardlechko.com",
+  "http://freedombutchers.richardlechko.com",
+  "https://freedombutchers.richardlechko.com",
+  "http://localhost:3000",
+];
 
-app.set("view engine", "php");
-app.set("views", path.join(__dirname, "freedombutchers"));
-
-// Use php-express engine
-app.engine("php", phpExpressEngine.engine);
-
-// Serve static files (HTML, CSS, JavaScript)
-app.use(express.static(path.join(__dirname, "freedombutchers")));
-
-/**************************************************/
-
-app.use("/About", express.static(path.join(__dirname, "About")));
-app.use("/Catering", express.static(path.join(__dirname, "Catering")));
-app.use("/Contact", express.static(path.join(__dirname, "Contact")));
-app.use("/Coupons", express.static(path.join(__dirname, "Coupons")));
 app.use(
-  "/CSS-Utilities",
-  express.static(path.join(__dirname, "CSS-Utilities"))
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
 );
-app.use("/Latest-News", express.static(path.join(__dirname, "Latest-News")));
-app.use("/Location", express.static(path.join(__dirname, "Location")));
-app.use("/Menu", express.static(path.join(__dirname, "Menu")));
-app.use("/proxy-server", express.static(path.join(__dirname, "proxy-server")));
-app.use("/Store-Hours", express.static(path.join(__dirname, "Store-Hours")));
-app.use("/Weekly-Sales", express.static(path.join(__dirname, "Weekly-Sales")));
-app.use("/", express.static(path.join(__dirname, "/")));
 
-// Define routes for each HTML page
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+app.use((req, res, next) => {
+  res.removeHeader("Permissions-Policy");
+  res.setHeader("Permissions-Policy", "interest-cohort=()");
+  res.cookie("yourCookieName", "yourCookieValue", {
+    sameSite: "None",
+    secure: true,
+  });
+  next();
 });
 
-app.get("/index.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+app.use(express.static(path.join(__dirname, "freedombutchers-main")));
+app.use(express.static(path.join(__dirname, "")));
+
+const htmlPages = [
+  "index.html",
+  "About/index.html",
+  "Catering/index.html",
+  "Contact/index.html",
+  "Location/index.html",
+  "Menu/index.html",
+  "Store-Hours/index.html",
+  "Weekly-Sales/index.html",
+];
+
+const directories = [
+  "About",
+  "Catering",
+  "Contact",
+  "Coupons",
+  "CSS-Utilities",
+  "Latest-News",
+  "Location",
+  "Menu",
+  "proxy-server",
+  "Store-Hours",
+  "Weekly-Sales",
+  "", // For serving static files from the root directory
+];
+
+directories.forEach((directory) => {
+  const route = directory === "" ? "/" : `/${directory}`;
+  app.use(route, express.static(path.join(__dirname, directory)));
 });
 
-app.get("/About/index.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "About", "index.html"));
+htmlPages.forEach((page) => {
+  const route = `/${page}`;
+  app.get(route, (req, res) => {
+    res.sendFile(path.join(__dirname, "freedombutchers-main", page));
+  });
 });
 
-app.get("/Catering/index.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "Catering", "index.html"));
-});
+// Handle contact form submissions
+app.post("/submit", async (req, res) => {
+  const { name, email, subject, message } = req.body;
 
-app.get("/Contact/index.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "Contact", "index.html"));
-});
-
-app.get("/Coupons/index.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "Coupons", "index.html"));
-});
-
-app.get("/Latest-News/index.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "Latest-News", "index.html"));
-});
-
-app.get("/Location/index.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "Location", "index.html"));
-});
-
-app.get("/Menu/index.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "Menu", "index.html"));
-});
-
-app.get("/Store-Hours/index.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "Store-Hours", "index.html"));
-});
-
-app.get("/Weekly-Sales/index.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "Weekly-Sales", "index.html"));
-});
-
-app.get("/api/reviews", async (req, res) => {
   try {
-    const response = await fetch("http://localhost:3001/api/reviews");
+    // Send email
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "richardlechko04@gmail.com", // Update with your email
+        pass: process.env.GMAIL_PASS, // Update with your email password or use environment variables
+      },
+    });
 
-    const data = await response.json();
-    res.json(data);
+    const mailOptions = {
+      from: "richardlechko04@gmail.com", // Update with your email address
+      to: email, // Use the email provided by the user
+      subject: subject,
+      text: `Name: ${name}\nEmail: ${email}\nSubject: ${subject}\nMessage: ${message}`,
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    // Define links to include in the response
+    const links = [];
+
+    // Send success response with links
+    res.status(200).json({ success: true, links });
   } catch (error) {
-    console.error("Error fetching Yelp reviews:", error);
-    res.status(500).json({ error: "Failed to fetch Yelp reviews" });
+    console.error("Error sending email:", error);
+
+    // Check for specific errors
+    if (error.code === "EAUTH") {
+      res.status(500).json({
+        success: false,
+        error: "Invalid email credentials. Please check your email settings.",
+      });
+    } else {
+      res.status(500).json({ success: false, error: "Failed to send email" });
+    }
   }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+const mainAppPort = process.env.MAIN_APP_PORT || 3000;
+http.createServer(app).listen(mainAppPort, () => {
+  console.log(`Server listening on port ${mainAppPort}`);
+});
+
+// Proxy server
+const proxyApp = express();
+
+proxyApp.use((req, res, next) => {
+  console.log(`Proxying request to: ${req.url}`);
+  next();
+});
+
+proxyApp.post("/submit", async (req, res) => {
+  try {
+    const response = await axios.post(
+      `http://localhost:${mainAppPort}${req.url}`,
+      req.body,
+      {
+        headers: req.headers,
+      }
+    );
+    res.status(response.status).send(response.data);
+  } catch (error) {
+    console.error("Error proxying request:", error);
+    res.status(500).send("Error proxying request");
+  }
+});
+
+const proxyPort = process.env.PROXY_PORT || 4000;
+proxyApp.listen(proxyPort, () => {
+  console.log(`Proxy server listening on port ${proxyPort}`);
 });
